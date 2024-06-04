5b0f77d2ccfda3b8b142c43529d4472818101ba2
Jakub Wąsik <144064944+wasikjakub@users.noreply.github.com>
2024-06-04 22:39:55 +0200

    Add files via upload

--- sketch.ino
+++ sketch.ino
@@ -1,23 +1,21 @@
 #include <LiquidCrystal.h>
 
-// Inicjalizacja wyświetlacza LCD
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-// Zmienne do przechowywania salda konta i cen produktów
+int beverageCount = 1;
+int snackCount = 5;
 int balance = 0;
-const int beveragePrice = 3; 
-const int snackPrice = 5; 
-int selectedProductPrice = snackPrice; 
-String selectedProduct = "Snack"; 
+const int beveragePrice = 3;
+const int snackPrice = 5;
+int selectedProductPrice = snackPrice;
+String selectedProduct = "Snack";
 
-// Piny przycisków i buzzera
 const int purchaseButtonPin = 2;
 const int coinButtonPin = 3;
 const int beverageButtonPin = 5;
 const int snackButtonPin = 6;
 const int buzzerPin = 4;
 
-// Zmienne do odczytu stanu przycisków
 int purchaseButtonState = 0;
 int coinButtonState = 0;
 int beverageButtonState = 0;
@@ -29,12 +27,11 @@ void setup() {
   pinMode(beverageButtonPin, INPUT_PULLUP);
   pinMode(snackButtonPin, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
-  lcd.begin(16, 2);
+  lcd.begin(20, 4);
   updateDisplay();
 }
 
 void loop() {
-  // Odczytanie stanu przycisków
   purchaseButtonState = digitalRead(purchaseButtonPin);
   coinButtonState = digitalRead(coinButtonPin);
   beverageButtonState = digitalRead(beverageButtonPin);
@@ -44,23 +41,52 @@ void loop() {
     selectedProductPrice = beveragePrice;
     selectedProduct = "Beverage";
     updateDisplay();
-    delay(500); 
+    delay(500);
   }
 
   if (snackButtonState == LOW) {
     selectedProductPrice = snackPrice;
     selectedProduct = "Snack";
     updateDisplay();
-    delay(500); 
-  }
-
-  if (purchaseButtonState == LOW && balance >= selectedProductPrice) {
-    balance -= selectedProductPrice;
-    dispensingAnimation();
-    updateDisplay();
     delay(500);
   }
- 
+
+  if (purchaseButtonState == LOW) {
+    if (selectedProduct == "Beverage" && beverageCount > 0 && balance >= selectedProductPrice) {
+      beverageCount--;
+      balance -= selectedProductPrice;
+      dispensingAnimation();
+      updateDisplay();
+      delay(500);
+    } else if (selectedProduct == "Snack" && snackCount > 0 && balance >= selectedProductPrice) {
+      snackCount--;
+      balance -= selectedProductPrice;
+      dispensingAnimation();
+      updateDisplay();
+      delay(500);
+    } else if (selectedProduct == "Beverage" && beverageCount == 0) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Out Of Stock!");
+      delay(1000);
+      updateDisplay();
+    } else if (selectedProduct == "Snack" && snackCount == 0) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Out Of Stock!");
+      delay(1000);
+      updateDisplay();
+    } else {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Please insert");
+      lcd.setCursor(0, 1);
+      lcd.print("correct amount");
+      delay(1000);
+      updateDisplay();
+    }
+  }
+
   if (coinButtonState == LOW) {
     balance++;
     updateDisplay();
@@ -68,7 +94,6 @@ void loop() {
   }
 }
 
-// Funkcja aktualizująca wyświetlacz
 void updateDisplay() {
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -80,20 +105,27 @@ void updateDisplay() {
   lcd.print(": ");
   lcd.print(selectedProductPrice);
   lcd.print(" PLN");
+  lcd.setCursor(0, 2);
+  lcd.print("Beverages: ");
+  lcd.print(beverageCount);
+  lcd.setCursor(0, 3);
+  lcd.print("Snacks: ");
+  lcd.print(snackCount);
 }
 
-// Funkcja wyświetlająca animację wydawania produktu
 void dispensingAnimation() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dispensing...");
 
   for (int i = 0; i < 3; i++) {
-    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Dispensing   ");
-    tone(buzzerPin, 500);  
+    tone(buzzerPin, 500);
     delay(500);
     lcd.setCursor(0, 0);
     lcd.print("Dispensing.  ");
-    noTone(buzzerPin);  
+    noTone(buzzerPin);
     delay(500);
     lcd.setCursor(0, 0);
     lcd.print("Dispensing.. ");
